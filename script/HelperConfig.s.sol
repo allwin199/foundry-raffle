281903d127247d6bfc3a2a4483ac6a22d00805f3
worksofallwin <princeallwin199@gmail.com>
2023-06-29 20:32:19 +0530

    Complete DeployRaffle and HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,5 +2,84 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
-contract HelperConfig is Script {}
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaEthConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, // 30seconds
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subscriptionId: 0,
+            callbackGasLimit: 500000 //500,000 gas
+        });
+        return sepoliaEthConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaEthConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x271682DEB8C4E0901D1a1550aD2e64D568E69909,
+            gasLane: 0x8af398995b04c28e9951adb9721ef74c74f93e6a478f39e7e0777be13527e7ef,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        });
+        return sepoliaEthConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        // In Solidity, the default value for a struct member of type address is address(0),
+        // which represents the Ethereum zero address or an uninitialized address.
+        // Therefore, the default value of vrfCoordinator in the NetworkConfig struct will be address(0).
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+        // If the address is detected as address(0)
+        // then it will go ahead and run the below code
+        // if not it will return already deployed mock.
+
+        // Deploying mocks to work on local chain
+        // If the address is address(0), this will execute
+        uint96 baseFee = 0.25 ether; // 0.25 LINK
+        uint96 gasPriceLink = 1e9; //1 GWEI LINK
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vRFCoordinatorV2Mock = new VRFCoordinatorV2Mock(
+            baseFee,
+            gasPriceLink
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilEthConfig = NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30, //30s
+            vrfCoordinator: address(vRFCoordinatorV2Mock),
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // dummy_one
+            subscriptionId: 0, // our script will add this!
+            callbackGasLimit: 500000
+        });
+        return anvilEthConfig;
+    }
+}
